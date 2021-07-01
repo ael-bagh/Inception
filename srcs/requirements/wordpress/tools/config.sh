4ca0c23393021cdc0b9dbc0a87a9c040d8e8e3d8
ael-bagh <ael-bagh@student.1337.ma>
2021-07-01 13:59:48 +0100

    sdsadasdasd

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -11,9 +11,9 @@ cp -r wp/* wordpress
 chown -R www-data /var/www/wordpress
 chmod -R 755 /var/www/wordpress/
 #replace necessary config using sed read about how it works here https://www.cyberciti.biz/faq/how-to-use-sed-to-find-and-replace-text-in-files-in-linux-unix-shell/
-# sed -i 's/database_name_here/'$DBNAME'/g' /var/www/wordpress/wp_config.php
-# sed -i 's/username_here/'$DBUSER'/g' /var/www/wordpress/wp_config.php
-# sed -i 's/password_here/'$DBPASS'/g' /var/www/wordpress/wp_config.php
+sed -i 's/database_name_here/'$DBNAME'/g' /var/www/wordpress/wp_config.php
+sed -i 's/username_here/'$DBUSER'/g' /var/www/wordpress/wp_config.php
+sed -i 's/password_here/'$DBPASS'/g' /var/www/wordpress/wp_config.php
 fi
 #run php-fpm in the foreground so it could be PID 1 in a docker container.
 #solution from here https://stackoverflow.com/questions/37313780/how-can-i-start-php-fpm-in-a-docker-container-by-default/44409813
@@ -21,7 +21,7 @@ fi
       #force to stay in foreground, and ignore daemonize option from config file
 #-R, --allow-to-run-as-root
       #Allow pool to run as root (disabled by default)
-# service php7.3-fpm start
-# service php7.3-fpm stop
-# php-fpm -F -R
-tail -f
+service php7.3-fpm start
+service php7.3-fpm stop
+php-fpm -F -R
+
