472f685068599f2cdeff0293492d4a5a41d016c0
ael-bagh <ael-bagh@student.1337.ma>
2021-07-01 13:29:56 +0100

    please work

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,3 +1,4 @@
+DIR="/var/www/wordpress/";
 apt-get update && apt-get upgrade -y
 apt -y install php7.3-fpm php7.3-common php7.3-mysql
 #https://www.cyberciti.biz/faq/linux-unix-shell-check-if-directory-empty
@@ -6,10 +7,13 @@ if [ "$(ls -A $DIR)" ]; then
 echo "$DIR isn't empty running php-fpm in the foreground"
 else
 echo "$DIR is Empty \n Copying and modifying wordpress"
+cp -r wp/* wordpress
+chown -R www-data /var/www/wordpress
+chmod -R 755 /var/www/wordpress/
 #replace necessary config using sed read about how it works here https://www.cyberciti.biz/faq/how-to-use-sed-to-find-and-replace-text-in-files-in-linux-unix-shell/
-sed -i 's/database_name_here/'$DBNAME'/g' /var/www/html/wp_config.php
-sed -i 's/username_here/'$DBUSER'/g' /var/www/html/wp_config.php
-sed -i 's/password_here/'$DBPASS'/g' /var/www/html/wp_config.php
+sed -i 's/database_name_here/'$DBNAME'/g' wordpress/wp_config.php
+sed -i 's/username_here/'$DBUSER'/g' wordpress/wp_config.php
+sed -i 's/password_here/'$DBPASS'/g' wordpress/wp_config.php
 fi
 #run php-fpm in the foreground so it could be PID 1 in a docker container.
 #solution from here https://stackoverflow.com/questions/37313780/how-can-i-start-php-fpm-in-a-docker-container-by-default/44409813
